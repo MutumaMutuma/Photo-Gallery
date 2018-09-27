bb1932a317e21e734d8f5a6129dd450a4e13a64d
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-27 14:03:03 +0300

    added static

--- pics/urls.py
+++ pics/urls.py
@@ -3,6 +3,6 @@ from . import views
 import datetime as dt
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^pics/$',views.mygallery,name='pics')
+    
+    url('',views.mygallery,name='pics')
 ]
