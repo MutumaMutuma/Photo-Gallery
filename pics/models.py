fa30381262ef2b50b2a169b6459da3a8151e4c70
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-27 15:53:37 +0300

    added a new class image

--- pics/models.py
+++ pics/models.py
@@ -3,14 +3,40 @@ from django.db import models
 
 # Create your models here.
 class Location(models.Model):
-   name = models.CharField(max_length=30)
 
-   def __str__(self):
-       return self.name
+    '''
+    locations model
+    '''
+    location = models.CharField(max_length=30)
+
+    def __str__(self):
+       return self.location
 
 class Category(models.Model):
+
     '''
     Photo category model
     '''
     
     name = models.CharField(max_length=120)
+
+    def __str__(self):
+       return self.name
+
+
+class Image(models.Model):
+
+    '''
+    Images model
+    '''
+    
+    image = models.ImageField(upload_to='gallery/', blank=True)
+    image_url = models.TextField(blank=False)
+    image_name = models.CharField(max_length=30, blank=False)
+    description = models.TextField(max_length=100, blank=False)
+    category = models.ManyToManyField(Category)
+    pub_date = models.DateTimeField(auto_now=True)
+    location = models.ForeignKey(Location)
+
+    def __str__(self):
+       return self.image_name
