d7f11a6dcf5140e25dd051987e462ab29a77917b
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-27 10:01:33 +0300

    added a templates

--- pics/views.py
+++ pics/views.py
@@ -3,4 +3,4 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to my Gallery')
+    return render(request, 'welcome.html')
