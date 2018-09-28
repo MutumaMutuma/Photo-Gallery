33ff11fe845f7dcfd295db575334c6f6b4fb68fe
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-28 18:36:32 +0300

    added a search view

--- pics/views.py
+++ pics/views.py
@@ -11,3 +11,15 @@ def mygallery(request):
     photos = Image.get_all()
     return render(request, 'all-pics/home.html', {"date": date, "photos":photos})
 
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_categorys = Category.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-pics/search.html',{"message":message,"categorys": searched_categorys})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-pics/search.html',{"message":message})
